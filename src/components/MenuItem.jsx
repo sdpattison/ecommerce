742c9e4c55c69ca27e7b12c9c52b299ca977e210
Sam Pattison <SamuelDPattison@gmail.com>
2021-06-19 12:10:07 -0400

    more modularization and added homepage css

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,8 +1,9 @@
 import "./menuitem.styles.scss";
 
-const MenuItem = ({ title })=>{
+const MenuItem = ({ title, imgUrl, size })=>{
     return(
-        <div className="menu-item">
+        <div className={`${size} menu-item`}>
+            <div className="background-image" style={{ backgroundImage:`url(${imgUrl}) `}}></div>
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,10 @@
-import MenuItem from "../components/MenuItem";
+import Directory from "../components/Directory";
 import "./homepage.styles.scss";
 
 const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <MenuItem title={"HATS"} />
-                <MenuItem title={"JACKETS"}/>
-                <MenuItem title={"SNEAKERS"}/>
-                <MenuItem title={"WOMENS"}/>
-                <MenuItem title={"MENS"}/>
-            </div>
+            <Directory />
         </div>
     );
 }
