1eb4d1bbb70eacac282b2deab76236ad926497ae
Sam Pattison <SamuelDPattison@gmail.com>
2021-06-20 14:29:40 -0400

    addition of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import HomePage from './pages/HomePage';
+import {Switch, Route} from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+      </Switch>
     </div>
   );
 }
