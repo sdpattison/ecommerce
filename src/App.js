1eb9d9310e58d83d47502c15cf8dbdbdb77db26e
Sam Pattison <SamuelDPattison@gmail.com>
2021-06-22 14:33:54 -0400

    functionality of adding google sign in users to DB

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Switch, Route} from "react-router-dom";
 import ShopPage from './pages/ShopPage/ShopPage';
 import Header from "./components/HeaderComponent/Header";
 import SignInLogIn from './pages/SignInPage/SignInLogIn';
-import {auth} from "./firebase/firebase.utils.js";
+import {auth, createUserProfileDocument} from "./firebase/firebase.utils.js";
 
 class App extends React.Component{
   constructor(){
@@ -16,8 +16,8 @@ class App extends React.Component{
   }
   unsubscribeFromAuth = null;
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user =>{
-      this.setState({currentUser: user});
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user =>{
+      createUserProfileDocument(user);
       console.log(user);
     })
   }
