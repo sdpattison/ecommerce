71e5d70196f592cc6a7eb8012f3132ac0b587bb6
sdpattison <samueldpattison@gmail.com>
2021-07-17 12:51:46 -0400

    remove item from cart redux logic

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,21 @@
+export const addItemToCart = ( cartItems, cartItemToAdd ) =>{
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToAdd.id
+    );
+    if(existingCartItem){
+        return cartItems.map(cartItem => 
+            cartItem.id === cartItemToAdd.id 
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : cartItem 
+        );
+    }
+
+    return [ ...cartItems, { ...cartItemToAdd, quantity: 1  }];
+}
+
+export const removeItemFromCart = ( cartItems, cartItemToRemove ) =>{
+    if(cartItemToRemove.quantity === 1){
+        return cartItems.filter(( cartItem => cartItem.id !== cartItemToRemove.id));
+    }
+    return [ ...cartItems, { ...cartItemToRemove, quantity: cartItemToRemove.quantity - 1}];
+}
