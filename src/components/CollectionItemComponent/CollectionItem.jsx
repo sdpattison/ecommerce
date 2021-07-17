c5e0976fcfd6f3bfa88797ecdad6a9953263b10a
sdpattison <samueldpattison@gmail.com>
2021-07-17 11:10:09 -0400

    add to cart custom button

--- src/components/CollectionItemComponent/CollectionItem.jsx
+++ src/components/CollectionItemComponent/CollectionItem.jsx
@@ -1,4 +1,5 @@
 import "./CollectionItem.styles.scss";
+import CustomButton from '../CustomButtonComponent/CustomButton';
 
 const CollectionItem = ({id, name, price, imageUrl}) =>{
     return(
@@ -9,6 +10,7 @@ const CollectionItem = ({id, name, price, imageUrl}) =>{
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
+            <CustomButton inverted> Add to cart </CustomButton>
         </div>
     );
 }
