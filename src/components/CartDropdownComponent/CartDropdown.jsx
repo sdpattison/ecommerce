e8cd42b4baf59eaf07850156df08464d75e945c8
sdpattison <samueldpattison@gmail.com>
2021-07-17 21:37:25 -0400

    adding ternary if the cart is empty

--- src/components/CartDropdownComponent/CartDropdown.jsx
+++ src/components/CartDropdownComponent/CartDropdown.jsx
@@ -4,17 +4,20 @@ import CartItem from '../CartItemComponent/CartItem';
 import { connect } from 'react-redux';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 
 
-const CartDropdown = ({ cartItems }) =>{
+const CartDropdown = ({ cartItems, history }) =>{
     return(
         <div className="cart-dropdown">
             <div className="cart-items">
                 {
-                    cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                    cartItems.length 
+                        ? cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+                        : <span className='empty-message'> Your cart is empty </span>
                 }
             </div>
-            <CustomButton> GO TO CHECKOUT </CustomButton>
+            <CustomButton onClick={()=> history.push('/checkout')}> GO TO CHECKOUT </CustomButton>
         </div>
     );
 }
@@ -23,4 +26,4 @@ const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
